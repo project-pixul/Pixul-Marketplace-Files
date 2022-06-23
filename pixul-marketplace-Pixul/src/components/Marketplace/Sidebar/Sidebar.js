8c56245a81ea24dc65f44d146bf274a962b7e908
Dunsin <jesudunsinfaiye@gmail.com>
2022-06-23 10:21:20 +0100

    done with the responsiveness of the home page

--- pixul-marketplace-Pixul/src/components/Marketplace/Sidebar/Sidebar.js
+++ pixul-marketplace-Pixul/src/components/Marketplace/Sidebar/Sidebar.js
@@ -2,13 +2,14 @@ import { Drawer } from "../../Drawer/Drawer";
 import { Nav } from "../../Nav/Nav";
 import { services } from "../../../API";
 
-export const Sidebar = ({ onClick }) => {
+import "../marketplace.css";
 
+export const Sidebar = ({ onClick }) => {
   return (
     <div>
       <Drawer items={services} onClick={onClick} />
-      <div className="hidden lg:block">
-        <h1 className="text-xl font-bold mb-3">Categories</h1>
+      <div>
+        <h1>Categories</h1>
         <Nav items={services} onClick={onClick} />
       </div>
     </div>
